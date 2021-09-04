46a509fd720c4277849ae5fd1882573de31c2213
benchpresslover <stariver0812@gmail.com>
2021-09-05 00:51:02 +0900

    Change html

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,10 @@ class Post(models.Model):
     tite = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    pubished_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.pubished_date = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
