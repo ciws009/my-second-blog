634edd12629601612626168d04a211cf8f80c767
benchpresslover <stariver0812@gmail.com>
2021-09-26 22:43:35 +0900

    Chagne settings

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,11 +10,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
-
 sys.path.append('/opt/bitnami/projects/mysite')
 
 os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/mysite/egg_cache")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+os.environ['HTTPS'] = "on"
+os.environ['wsgi.url_scheme'] = 'https'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
