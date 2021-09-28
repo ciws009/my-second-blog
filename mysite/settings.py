54f4a6c8b49470cb7e2cf67ffe08bf9d646c85f6
benchpresslover <stariver0812@gmail.com>
2021-09-29 00:50:55 +0900

    Add css

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ol2kq)!u_r@7mbzap@c0sp(3a951muo4rv3%2xsuoh+r#7_p8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 
