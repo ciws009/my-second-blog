c227fa924367e2327a65c8430d458c02c0c73ca9
benchpresslover <stariver0812@gmail.com>
2022-03-01 22:32:15 +0900

    test

--- updator/updator.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from updator import post_api
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(post_api.update_related_post_titles, 'interval', minutes=10)
-    scheduler.start()
